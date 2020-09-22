f87a0313003a370f20e820b208e012592f0e7ffb
Dirk Wulff <dirkwulff@fp-ws-b210.psycho.unibas.ch>
2020-09-22 16:02:19 +0200

    ..

--- _sessions/analyze_participation.R
+++ _sessions/analyze_participation.R
@@ -3,11 +3,32 @@ Teilnehmer = readr::read_csv('_sessions/Teilnehmer.csv')
 Bernoulli = readr::read_csv('_sessions/BernoulliPascal/Bernoulli.csv')
 Pascal = readr::read_csv('_sessions/BernoulliPascal/Pascal.csv')
 
-d = e %>% 
-  left_join(a, by = c("Last name" = "Nachname","First name" = "Vorname")) %>% 
-  left_join(b, by = c("Last name" = "Nachname","First name" = "Vorname"))
+Teilnehmer = Teilnehmer %>% 
+  left_join(Bernoulli, 
+            by = c("Last name" = "Nachname","First name" = "Vorname")) %>% 
+  left_join(Pascal, 
+            by = c("Last name" = "Nachname","First name" = "Vorname"))
 
-d %>% select(`Last name`,`First name`,`Aktive Lesezeit.x`,`Aktive Lesezeit.y`) %>% print(n = 100)
+Teilnehmer %>% 
+  select(`Last name`,`First name`,`Aktive Lesezeit.x`,`Aktive Lesezeit.y`) %>% print(n = 100)
 
-1.5 * 12
-60 / 14
+
+encode = function(x) charToRaw(paste0(base::rev(str_split(x,'')[[1]]),collapse=''))
+
+names = paste(Teilnehmer$`First name`, Teilnehmer$`Last name`)
+names_code = sapply(names, encode)
+
+set.seed(100)
+rnd = sample(1:27)
+
+gr_1 = names_code[1:9]
+gr_2 = names_code[10:18]
+gr_3 = names_code[19:27]
+
+names(gr_1)
+names(gr_2)
+names(gr_3)
+
+paste0('"',gr_1,'"')
+
+
